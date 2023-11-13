63dafd0ffb6cfbf88006918e9883e5b5158ccf40
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-13 16:26:34 -0300

    Update lesson 4
    Update lesson 4

--- freecodecampprojects/src/App.js
+++ freecodecampprojects/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const jsx= <div></div>;
+
 function App() {
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <jsx><h1>oi</h1>
+    <p></p>
+  <ul>
+  <li></li>
+  <li></li>
+  <li></li>
+  </ul></jsx>
     </div>
   );
 }
