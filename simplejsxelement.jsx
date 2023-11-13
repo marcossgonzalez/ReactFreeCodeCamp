a27147c1e3b2935a5f5b3941073a85c74e71bfb7
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-13 14:29:24 -0300

    Minor update
    minor update

--- simplejsxelement.jsx
+++ simplejsxelement.jsx
@@ -1,1 +1,6 @@
 const JSX = <h1>Hello JSX!</h1>;
+/*Por exemplo, como JSX é uma extensão sintática de JavaScript, você pode 
+escrever JavaScript diretamente dentro de JSX. Para fazer isso, basta incluir
+ o código que deseja que seja tratado como JavaScript entre chaves: { 'este é tratado como código JavaScript' }. 
+ Lembre-se disso, pois será utilizado em diversos desafios futuros.
+*/
