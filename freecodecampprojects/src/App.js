721dab064aee5ffab83d1dc1afcacbfb66199752
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-20 00:22:52 -0300

    Update lesson 4
    Update lesson 4

--- freecodecampprojects/src/App.js
+++ freecodecampprojects/src/App.js
@@ -272,16 +272,35 @@ Por exemplo:
 MyComponent.defaultProps = { location: 'San Francisco' }
 A definição padrão para a propriedade do MyComponent será de `Sao Francisco`
 */
-const ShoppingCart = (props) => {
+//const ShoppingCart = (props) => {
   
-  return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-    </div>
-  )
-};
+  //return (
+    //<div>
+      //<h1>Shopping Cart Component</h1>
+    //</div>
+  //)
+//};
 // Change code below this line
-ShoppingCart.defaultProps = { items: 0};
+//ShoppingCart.defaultProps = { items: 0};
+//-----------------------------------------------
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    { /* Change code below this line */ }
+    return <Items quantity={10}/>
+    { /* Change code above this line */ }
+  }
+};
 
 
 //ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
